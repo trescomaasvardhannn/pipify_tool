92f94b03b7021bda20c04dc1b03f0c66da2eb296
vardhannn5114 <124501395+trescomaasvardhannn@users.noreply.github.com>
2024-06-22 12:09:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+async function startScreenCapture() {
+    try {
+        const constraints = {
+            mediaSource: "screen",
+            audio: false
+            };
+
+        const stream = await navigator.mediaDevices.getDisplayMedia(constraints);
+        const video = document.createElement('video');
+        video.srcObject = stream;
+        video.style.display = 'none';
+        document.body.appendChild(video);
+
+        await new Promise((resolve) => {
+            video.onloadedmetadata = async () => {
+                await video.play();
+                resolve();
+            };
+        });
+
+      
+
+        if (document.pictureInPictureEnabled) {
+            try {
+                await video.requestPictureInPicture();
+                console.log('Entered Picture-in-Picture mode');
+            } catch (error) {
+                console.error('Error entering Picture-in-Picture mode:', error);
+            }
+        } else {
+            console.error('Picture-in-Picture is not supported in this browser.');
+        }
+    } catch (error) {
+        console.error('Error capturing screen:', error);
+    }
+}
+
+document.getElementById('captureEntireScreen').addEventListener('click', async () => {
+    await startScreenCapture();
+});
